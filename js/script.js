7dc847b8afb3bc003edbf5538272b873dad51eee
Ivan <miniglez.98@gmail.com>
2024-06-15 21:06:50 +0200

    Ejercicio terminado

--- js/script.js
+++ js/script.js
@@ -1,2 +1,44 @@
 import peliculas from './peliculas.js'
 
+const actionHTML = document.getElementById("genero-28");
+const thrillerHTML = document.getElementById("genero-53")
+const adventureHTML = document.getElementById("genero-12")
+
+const actionList = addFilmToList(28)
+const thrillerList = addFilmToList(53)
+const adventureList = addFilmToList(12)
+
+
+addFilmToHTML(actionHTML, actionList)
+addFilmToHTML(thrillerHTML, thrillerList)
+addFilmToHTML(adventureHTML, adventureList)
+
+
+function addFilmToHTML (elementHTML, objectToAdd){
+    for (let i of objectToAdd){
+        const elementDIV = document.createElement("div")
+        const elementIMG = document.createElement("img")
+        //const elementP = document.createTextNode(i["original_title"])
+        const elementP = document.createElement("p")
+
+        elementDIV.className = "peliculas"
+        elementIMG.src = "https://image.tmdb.org/t/p/w500" + i["poster_path"]
+        elementP.innerHTML = i["original_title"]
+        elementDIV.appendChild(elementIMG);
+        elementDIV.appendChild(elementP);
+        console.log(elementDIV)
+        
+        elementHTML.appendChild(elementDIV)
+    }
+    console.log(elementHTML)
+}
+
+function addFilmToList(a){
+    const arr = []
+    for (let i of peliculas){
+        if (i.genre_ids.find((element) => element == a)){
+            arr.push(i)
+        }
+    }
+    return arr
+}
